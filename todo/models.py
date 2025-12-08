f971a75350348f353bda884a0b249d447889a2e5
Sumaiya Chan <Sumaiyachan2308@gmail.com>
2025-12-08 10:49:45 +0530

    My first save: Website is working with add, edit, delete, toggle

---
+++ todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str(self):
+        return self.title
+
